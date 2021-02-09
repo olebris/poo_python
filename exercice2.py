c4b91e7ddad2d30778f9c7395b893249690a4c93
OLB_ <olivier@lebris.name>
2021-02-10 00:29:26 +0100

    init branch

---
+++ exercice2.py
@@ -0,0 +1,11 @@
+passdict= {"alecoutur":"Huz4","lmartin":"monty","fsincere":"gnugpl"}
+print (passdict)
+login=input("tape ton login ")
+pwd=input("tape ton pwd ")
+for lgin,ppwd in passdict.items():
+    #print(lgin,ppwd)
+    if lgin==login and ppwd==pwd:
+        bool_log= True
+        break
+    else: bool_log = False
+print(bool_log)
